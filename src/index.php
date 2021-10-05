56da066441f9eb5a79d53a5f4cdae781434b514b
Alex Ivanov <alexivanovrus@gmail.com>
2021-10-05 23:31:41 +0300

    javascript validation

--- src/index.php
+++ src/index.php
@@ -2,19 +2,31 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Тестируем PHP</title>
+    <title>Веб-программирование</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="index.css">
 </head>
 <body>
-<?php echo '<p>Привет, мир!</p>'; ?>
+<header>
+    <h1>Лабораторная работа №1</h1>
+    <ul>
+        <li>Студент: <span data-name="alex">Иванов Алексей Анатольевич</span></li>
+        <li>Группа: <span class="cursive">P3211</span></li>
+        <li>Вариант: 11008</li>
+    </ul>
+</header>
+<p class="green-box">
+    Разработать PHP-скрипт, определяющий попадание точки на координатной плоскости в заданную область,
+    и создать HTML-страницу, которая формирует данные для отправки их на обработку этому скрипту
+</p>
 <img alt="зона для тестирования попадания" src="areas.jpg" class="area"/>
 <form id="hitDataForm" action="/hit.php" method="post">
     <label for="xInput">x</label>
-    <input id="xInput" name="x" type="number"/>
+    <input id="xInput" name="x" />
     <label for="yInput">y</label>
-    <input id="yInput" name="y" type="number"/>
+    <input id="yInput" name="y" />
     <label for="radiusInput">radius</label>
-    <input id="radiusInput" name="r" type="number"/>
+    <input id="radiusInput" name="r"/>
     <button type="submit">submit</button>
 </form>
 <script src="validation.js"></script>
--- src/validation.js
+++ src/validation.js
@@ -1,15 +1,44 @@
 let hitDataForm = document.querySelector("#hitDataForm");
 hitDataForm.addEventListener("submit", validate)
 
-function validate(e) {
-    let x = hitDataForm.querySelector("#x").value
+function validate(event) {
+    let xAsStr = hitDataForm.querySelector("#xInput").value
+    let yAsStr = hitDataForm.querySelector("#yInput").value
+    let radiusAsString = hitDataForm.querySelector("#radiusInput").value
 
-    if (x > 10) {
-        e.preventDefault()
+    let x = parseNumber(xAsStr);
+    if (x === null) {
+        alert("x: " + xAsStr + " не является числом")
+        event.preventDefault();
         return;
     }
 
+    let y = parseNumber(yAsStr);
+    if (y === null) {
+        alert("y: " + yAsStr + " не является числом")
+        event.preventDefault();
+        return;
+    }
 
-    // let y = hitDataForm.getElementById("y")
-    // let r = hitDataForm.getElementById("r")
+    let radius = parseNumber(radiusAsString);
+    if (radiusAsString === null) {
+        alert("radius: " + radiusAsString + " не является числом")
+        event.preventDefault();
+        return;
+    }
+
+    if (radius <= 0) {
+        alert("радиус должен быть больше 0")
+        event.preventDefault();
+        return;
+    }
+}
+
+function parseNumber(str) {
+    let value = Number.parseInt(str);
+    if (isNaN(value)) {
+        return null;
+    } else {
+        return value;
+    }
 }
