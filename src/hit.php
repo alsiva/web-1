790c25e3e7397a8feb4ce8125b85e1989d22f2e1
Alex Ivanov <alexivanovrus@gmail.com>
2021-10-06 17:52:16 +0300

    check hit and show table

--- src/hit.php
+++ src/hit.php
@@ -4,10 +4,25 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="hit.css">
     <title>Тестируем попадание</title>
 </head>
 <body>
 <?php
+class HitData {
+    public $x;
+    public $y;
+    public $r;
+    public $hit;
+
+    public function __construct($x, $y, $r, $hit) {
+        $this->x = $x;
+        $this->y = $y;
+        $this->r = $r;
+        $this->hit = $hit;
+    }
+}
+
 $x = $_POST['x'];
 $y = $_POST['y'];
 $r = $_POST['r'];
@@ -15,14 +30,38 @@ $r = $_POST['r'];
 require __DIR__ . '/calculation.php';
 
 $hit = doesItHit($x, $y, $r);
+$hitData = new HitData($x, $y, $r, $hit);
+
+$hits = array(
+    $hitData,
+)
 ?>
 
-<p>
-<?php if ($hit == true): ?>
-    Попадание есть
-<?php else: ?>
-    Попадания нет
-<?php endif; ?>
-</p>
+<table>
+<thead>
+<tr>
+    <th>x:</th>
+    <th>y:</th>
+    <th>radius:</th>
+    <th>Попадание:</th>
+</tr>
+</thead>
+
+<tbody id="tableData"> </tbody>
+    <?php foreach($hits as $hitData): ?>
+        <tr>
+            <td><?= $hitData->x; ?></td>
+            <td><?= $hitData->y; ?></td>
+            <td><?= $hitData->r; ?></td>
+            <td>
+                <?php if ($hitData->hit == true): ?>
+                    Попадание есть
+                <?php else: ?>
+                    Попадания нет
+                <?php endif; ?>
+            </td>
+        </tr>
+    <?php endforeach; ?>
+</table>
 </body>
 </html>
